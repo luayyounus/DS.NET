9d875246c8a0e61478f424e98c7802c6e0e3c3a6
Luay <luayghazwan@yahoo.com>
2017-12-19 18:57:25 -0800

    Reverse String without Reverse method code challenge

---
+++ CodeChallenges/CodeChallenges/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CodeChallenges
+{
+    class FizzBuzz
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"FizzBuzz for number 1: {CheckNumber(1)}");
+            Console.ReadLine();
+            Console.WriteLine($"FizzBuzz for Checking number 3: {CheckNumber(3)}");
+            Console.ReadLine();
+            Console.WriteLine($"FizzBuzz for Checking number 15: {CheckNumber(15)}");
+            Console.ReadLine();
+        }
+
+        static string CheckNumber(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+                return "FizzBuzz";
+            if (num % 3 == 0)
+                return "Fizz";
+            if (num % 5 == 0)
+                return "Buzz";
+
+            return $"{num} is non-divisible";
+        }
+    }
+}
---
+++ CodeChallenges/CodeChallenges/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenges
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Reversed word of 'STAR' is: {Reverse("STAR")}");
+            Console.ReadLine();
+        }
+
+        static string Reverse(string word)
+        {
+            char[] wordArray = word.ToCharArray();
+            char[] reverseArray = new char[word.Length];
+            for (int i = 0, j = word.Length -1; i < word.Length; i++, j--)
+            {
+                reverseArray[i] = word[j];
+            }
+            return new String(reverseArray);
+        }
+    }
+}
