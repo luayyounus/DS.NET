7089fae2f28bc255d68a882e166e76ba208446fe
Luay <luayghazwan@yahoo.com>
2017-12-22 20:40:08 -0800

    Array occurances times code challenge

---
+++ CodeChallenges/CodeChallenges/occurancesCount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+
+namespace CodeChallenges
+{
+    class OccurancesCount
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = { 1, 1, 3, 3, 3, 4, 4, 5, 6, 6 };
+            PrintOccurances(numArray);
+            Console.ReadLine();
+        }
+
+        static void PrintOccurances(int[] input)
+        {
+            int counter = 1;
+            int prev, curr;
+            prev = input[0];
+            curr = input[1];
+
+            int index = 1;
+            while (index < input.Length -1)
+            {
+                if (curr != prev)
+                {
+                    Console.WriteLine($"{prev}:{counter} occurances");
+                    counter = 0;
+                }
+                counter++;
+                prev = curr;
+                curr = input[++index];
+            }
+            Console.WriteLine($"{prev}:{++counter} occurances");
+        }
+    }
+}
