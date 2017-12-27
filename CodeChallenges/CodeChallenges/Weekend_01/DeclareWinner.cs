8106e68259230dd9e928d5685ba91b8d08b7fb59
Luay <luayghazwan@yahoo.com>
2017-12-26 17:00:44 -0800

    Tested with main method

--- CodeChallenges/CodeChallenges/Weekend_01/DeclareWinner.cs
+++ CodeChallenges/CodeChallenges/Weekend_01/DeclareWinner.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CodeChallenges.Weekend_01
+namespace CodeChallenges
 {
-    public class Fighter
+    // Kata on Code Wars https://www.codewars.com/kata/two-fighters-one-winner 
+    class Fighter
     {
         public string Name;
         public int Health, DamagePerAttack;
@@ -16,24 +17,29 @@ namespace CodeChallenges.Weekend_01
         }
     }
 
-    class War
+    class DeclareWinner
     {
-        public class Kata
+        private static void Main(string[] args)
         {
-            public static string DeclareWinner(Fighter fighter1, Fighter fighter2, string firstAttacker)
+            Fighter fighterOne = new Fighter("Luay", 10, 4);
+            Fighter fighterTwo = new Fighter("Csharp", 10, 2);
+            string resultWinner = CheckWinner(fighterOne, fighterTwo, "Luay");
+            Console.WriteLine("The winner is {0}", resultWinner);
+            Console.ReadLine();
+        }
+        public static string CheckWinner(Fighter fighter1, Fighter fighter2, string firstAttacker)
+        {
+            string currentAttacker = firstAttacker;
+            while (fighter1.Health > 0 && fighter2.Health > 0)
             {
-                string currentAttacker = firstAttacker;
-                while (fighter1.Health > 0 && fighter2.Health > 0)
-                {
-                    if (currentAttacker == fighter1.Name)
-                        fighter2.Health -= fighter1.DamagePerAttack;
-                    else
-                        fighter1.Health -= fighter2.DamagePerAttack;
+                if (currentAttacker == fighter1.Name)
+                    fighter2.Health -= fighter1.DamagePerAttack;
+                else
+                    fighter1.Health -= fighter2.DamagePerAttack;
 
-                    currentAttacker = (currentAttacker == fighter2.Name) ? fighter1.Name : fighter2.Name;
-                }
-                return (fighter1.Health <= 0) ? fighter2.Name : fighter1.Name;
+                currentAttacker = (currentAttacker == fighter2.Name) ? fighter1.Name : fighter2.Name;
             }
+            return (fighter1.Health <= 0) ? fighter2.Name : fighter1.Name;
         }
     }
 }
