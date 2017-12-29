eead06d381d3de39a9de7c4ed486bc9681e9f22e
Luay <luayghazwan@yahoo.com>
2017-12-28 19:09:55 -0800

    Add Node to the First location in a Linked List

---
+++ CodeChallenges/CodeChallenges/Week_02/SLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenges.Week_02
+{
+    public class SLL
+    {
+        public SLLNode Head = new SLLNode();
+
+        // Adding int value to the first position in a LinkedList
+        public SLLNode AddFirst(int value)
+        {
+            SLLNode newNode = new SLLNode { Value = value };
+            if (Head == null) return newNode;
+            newNode.Next = Head;
+            Head = newNode;
+            return Head;
+        }
+    }
+}
