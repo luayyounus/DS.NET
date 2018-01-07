6ffcec571284cdfd7c0a4d27fcd95d8788f84dd0
Luay <luayghazwan@yahoo.com>
2018-01-06 19:28:10 -0800

    Replace alphabet with their position in the alphabet order

---
+++ CodeChallenges/CodeChallenges/Weekend_02_03/ReplaceWithAlphabetPosition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeChallenges.Weekend_02_03
+{
+    class ReplaceWithAlphabetPosition
+    {
+        public static void Main(string[] args)
+        {
+            string result = AlphabetPosition("Micky mouse lives in Hollywood. right?");
+            Console.WriteLine(result);
+
+            string result2 = AlphabetPositionWithLinq("Micky mouse lives in Hollywood. right?");
+            Console.WriteLine(result2);
+            Console.Read();
+        }
+
+        // 6 kyu kata
+        public static string AlphabetPosition(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in text.ToLower())
+            {
+                int charValue = (int) c;
+                if (charValue >= 97 && charValue <= 122)
+                {
+                    sb.Append(charValue - 96 + " ");
+                }
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string AlphabetPositionWithLinq(string text)
+        {
+            return string.Join(" ", text.ToLower().Where(char.IsLetter).Select(c => c - 96));
+        }
+    }
+}
