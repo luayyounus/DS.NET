edd14a5bcf9d7ee46824a1a2e77bca9c6ec06169
Luay <luayghazwan@yahoo.com>
2018-01-07 01:58:34 -0800

    Playing With Pass Phrase code challenge

---
+++ CodeChallenges/CodeChallenges/Weekend_02_03/PlayPass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeChallenges.Weekend_02_03
+{
+    class PlayPass
+    {
+        public static void Main(string[] args)
+        {
+            string result = PlayPassphrase("MY GRANMA CAME FROM NY ON THE 23RD OF APRIL 2015", 2);
+            Console.WriteLine(result);
+            
+            Console.Read();
+        }
+
+        // Kata https://www.codewars.com/kata/playing-with-passphrases/train/csharp
+        public static string PlayPassphrase(string s, int n)
+        {
+            char[] complement = new[] { '9', '8', '7', '6', '5', '4', '3', '2', '1', '0'};
+            char[] charArray = s.ToLower().ToCharArray();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLetter(charArray[i]))
+                {
+                    int ascii = charArray[i] + n;
+                    if (ascii > 122) ascii -= 26;
+                    if (i % 2 == 0)
+                        charArray[i] = char.ToUpper((char) ascii);
+                    else
+                        charArray[i] = char.ToLower((char)ascii);
+                }
+                if (char.IsDigit(charArray[i]))
+                    charArray[i] = complement[charArray[i] - '0'];
+            }
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
