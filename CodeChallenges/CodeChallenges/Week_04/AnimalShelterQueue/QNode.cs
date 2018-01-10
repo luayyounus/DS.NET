77e95a9b2e19e9bb1727c00ee6f1a3cf4b6fa66e
Luay <luayghazwan@yahoo.com>
2018-01-09 17:53:46 -0800

    Queue for cat and dogs shelter

---
+++ CodeChallenges/CodeChallenges/Week_04/AnimalShelterQueue/QNode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallenges.Week_04.AnimalShelterQueue
+{
+    public class QNode
+    {
+        public string AnimalName;
+        public QNode Next;
+    }
+}
