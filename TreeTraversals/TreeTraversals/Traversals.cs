16616ad013615adc5a9fea34ef6e88e526f83885
Luay <luayghazwan@yahoo.com>
2018-01-16 17:54:09 -0800

    Tree Traversals

---
+++ TreeTraversals/TreeTraversals/Traversals.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeTraversals
+{
+    public class Traversals
+    {
+        public TNode Root = null;
+
+        public void PreOrderTraversal(TNode root)
+        {
+            if (root == null) return;
+
+            Console.WriteLine(root.Val);
+            if (root.Left != null)
+                PostOrderTraversal(root.Left);
+            if (root.Right != null)
+                PostOrderTraversal(root.Right);
+        }
+
+        public void InOrderTraversal(TNode root)
+        {
+            if (root == null) return;
+            if (root.Left != null)
+                InOrderTraversal(root.Left);
+            Console.WriteLine(root.Val);
+            if (root.Right != null)
+                InOrderTraversal(root.Right);
+        }
+        public void PostOrderTraversal(TNode root)
+        {
+            if (root == null) return;
+            if (root.Left != null)
+                PostOrderTraversal(root.Left);
+            if (root.Right != null)
+                PostOrderTraversal(root.Right);
+            Console.WriteLine(root.Val);
+        }
+    }
+}
