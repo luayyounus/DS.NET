2cb30bbb0890e80711946a0daaec8fc4f2098adc
Luay <luayghazwan@yahoo.com>
2018-01-19 21:12:49 -0800

    Amazon Practice

---
+++ AmazonPractice/AmazonPractice/AddTwoNumbers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmazonPractice
+{
+    class AddTwoNumbers
+    {
+        //public static void Main(string[] args)
+        //{
+        //    //Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+
+        //    ListNode L1 = new ListNode(2) {next = new ListNode(4) {next = new ListNode(3)}};
+
+        //    ListNode L2 = new ListNode(5) {next = new ListNode(6) {next = new ListNode(4)}};
+
+        //    ListNode result = nAddTwoNumbers(L1, L2);
+        //}
+        //Definition for singly-linked list.
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+        public static ListNode nAddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            string num1 = "";
+            string num2 = "";
+
+            while (l1 != null)
+            {
+                num1 = l1.val.ToString() + num1;
+                l1 = l1.next;
+            }
+
+            while (l2 != null)
+            {
+                num2 = l2.val.ToString() + num2;
+                l2 = l2.next;
+            }
+
+            int sum = int.Parse(num1) + int.Parse(num2);
+            int firstNum = sum % 10;
+
+            sum = sum / 10;
+
+            ListNode head = new ListNode(firstNum);
+            ListNode temp = head;
+            while (sum != 0)
+            {
+                ListNode newNode = new ListNode(sum % 10);
+                temp.next = newNode;
+                temp = temp.next;
+                sum = sum / 10;
+            }
+
+            return head;
+        }
+    }
+    
+}
---
+++ AmazonPractice/AmazonPractice/MaximumSizeSubarraySumEqualsK.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmazonPractice
+{
+    internal class MaximumSizeSubarraySumEqualsK
+    {
+        public static int MaxSubArrayLengthFaster(int[] nums, int k)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int max = 0;
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if (sum == k) max = Math.Max(max, i + 1);
+                int diff = sum - k;
+                if (dict.ContainsKey(diff)) max = Math.Max(max, i - dict[diff]);
+                if (!dict.ContainsKey(sum)) dict.Add(sum, i);
+            }
+            return max;
+        }
+
+        public static int MaxSubArrayLen(int[] nums, int k)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+            int result = 0;
+            Dictionary<int, int> dict = new Dictionary<int, int> {{0, -1}};
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] += nums[i - 1];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i] - k))
+                {
+                    result = Math.Max(result, i - dict[(nums[i] - k)]);
+                }
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], i);
+                }
+            }
+            return result;
+        }
+    }
+}
