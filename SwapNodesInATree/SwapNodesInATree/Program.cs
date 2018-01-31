3019bab4c1454dabc584a7a1df9d4d7dfe521423
Luay <luayghazwan@yahoo.com>
2018-01-30 23:23:41 -0800

    Recursion Solution

--- SwapNodesInATree/SwapNodesInATree/Program.cs
+++ SwapNodesInATree/SwapNodesInATree/Program.cs
@@ -8,33 +8,36 @@ namespace SwapNodesInATree
     {
         static void Main(string[] args)
         {
-            Tree t = new Tree
-            {
-                Root = new Node
-                {
-                    Value = 2,
-                    Left = new Node
-                    {
-                        Value = 4,
-                        Left = new Node {Value = 3},
-                        Right = new Node {Value = 10}
-                    },
-                    Right = new Node
-                    {
-                        Value = 9,
-                        Right = new Node {Value = 1},
-                        Left = new Node {Value = 5}
-                    }
-                }
-            };
+            Node root = new Node(4);
+            Node left = new Node(2);
+            Node right = new Node(6);
+            Node leftLeft = new Node(3);
+            Node leftRight = new Node(5);
+            Node rightLeft = new Node(1);
+            Node rightRight = new Node(7);
 
+            root.Left = left;
+            root.Right = right;
+            root.Left.Left = leftLeft;
+            root.Left.Right = leftRight;
+            root.Right.Left = rightLeft;
+            root.Right.Right = rightRight;
+
+
+            Tree t = new Tree();
+            t.Root = root;
+            t.GetTwoNodesArray(t.Root, 7, 5);
+
+            // output 1
             Console.WriteLine("Before the swap");
-            t.OutputTreeInOrder(t.Root);
+            t.InOrderTraversal(t.Root);
 
-            t.SwapWithBreadth(1, 3);
 
+            t.SwapTwoLeaves(t.Root);
+
+            // output 2
             Console.WriteLine("After the swap");
-            t.OutputTreeInOrder(t.Root);
+            t.InOrderTraversal(t.Root);
 
             Console.ReadLine();
         }
@@ -42,46 +45,63 @@ namespace SwapNodesInATree
 
     public class Tree
     {
-        public Node Root;
+        public Node Root = null;
 
-        public void SwapWithBreadth(int a, int b)
+        public Node[] TwoNodes = new Node[2];
+
+        public void SwapTwoLeaves(Node root)
         {
-            Queue<Node> q = new Queue<Node>();
-            Node nodeToSwapOne = null;
-            Node nodeToSwapTwo = null;
-            q.Enqueue(Root);
-            while (q.Count > 0)
+            if (root == null) return;
+            if (root.Left != null && root.Left.Value == TwoNodes[0].Value)
             {
-                Node temp = q.Dequeue();
-                if (temp.Left == null && temp.Right == null)
-                {
-                    if (temp.Value == a) nodeToSwapOne = temp;
-                    if (temp.Value == b) nodeToSwapTwo = temp;
-                }
-                if (nodeToSwapOne != null && nodeToSwapTwo != null)
-                {
-                    Node actualSwap = nodeToSwapOne;
-                    nodeToSwapOne = nodeToSwapTwo;
-                    nodeToSwapTwo = actualSwap;
-                    return;
-                }
-                if(temp.Left != null) q.Enqueue(temp.Left);
-                if(temp.Right != null) q.Enqueue(temp.Right);
+                root.Left = TwoNodes[1];
+                Node temp = TwoNodes[0];
+                TwoNodes[0] = TwoNodes[1];
+                TwoNodes[1] = temp;
             }
+
+            if (root.Right != null && root.Right.Value == TwoNodes[0].Value)
+            {
+                root.Right = TwoNodes[1];
+                Node temp = TwoNodes[0];
+                TwoNodes[0] = TwoNodes[1];
+                TwoNodes[1] = temp;
+            }
+            if (root.Left != null && root.Left.Value == TwoNodes[1].Value)
+            {
+                root.Left = TwoNodes[0];
+                Node temp = TwoNodes[0];
+                TwoNodes[0] = TwoNodes[1];
+                TwoNodes[1] = temp;
+            }
+            if (root.Right != null && root.Right.Value == TwoNodes[1].Value)
+            {
+                root.Right = TwoNodes[0];
+                Node temp = TwoNodes[0];
+                TwoNodes[0] = TwoNodes[1];
+                TwoNodes[1] = temp;
+            }
+
+            SwapTwoLeaves(root.Left);
+            SwapTwoLeaves(root.Right);
         }
 
-        public void OutputTreeInOrder(Node root)
+        public void GetTwoNodesArray(Node root, int a, int b)
         {
-            if(root.Left != null) OutputTreeInOrder(root.Left);
-            Console.WriteLine(root.Value);
-            if(root.Right != null) OutputTreeInOrder(root.Right);
+            if (root == null) return;
+            if (root.Value == a) TwoNodes[0] = root;
+            if (root.Value == b) TwoNodes[1] = root;
+            GetTwoNodesArray(root.Left, a, b);
+            GetTwoNodesArray(root.Right, a, b);
         }
 
-        //public void SwapNodes(Node root, Node a, Node b)
-        //{
-        //    if(root.Value != a.Value || root.Value != b.Value && root.Left != null) SwapNodes(root.Left, a, b);
-        //    if(root.Value != ) SwapNodes(root.Right, a, b);
-        //}
+        public void InOrderTraversal(Node root)
+        {
+            if (root == null) return;
+            InOrderTraversal(root.Left);
+            Console.WriteLine(root.Value);
+            InOrderTraversal(root.Right);
+        }
 
     }
 
@@ -90,5 +110,9 @@ namespace SwapNodesInATree
         public int Value;
         public Node Left;
         public Node Right;
+        public Node(int x)
+        {
+            Value = x;
+        }
     }
 }
